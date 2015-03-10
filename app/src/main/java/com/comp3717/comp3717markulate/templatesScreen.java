f4e69d77399080e1f3b5fc04343ef607a6fb9ce1
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-03-10 15:11:46 -0700

    templates_screen activity is added

--- app/src/main/java/com/comp3717/comp3717markulate/templatesScreen.java
+++ app/src/main/java/com/comp3717/comp3717markulate/templatesScreen.java
@@ -1,25 +1,24 @@
 package com.comp3717.comp3717markulate;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class templatesScreen extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_templates_screen);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_templates_screen, menu);
         return true;
     }
 
@@ -36,9 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void templates_screen(View v) {
-        startActivity(new Intent(getApplicationContext(), templatesScreen.class));
     }
 }
